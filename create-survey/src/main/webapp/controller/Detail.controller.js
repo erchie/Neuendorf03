84c9ed32c10b6fab8ba3b20b874cf03295ead9d4
Schuster <wn00096217@wgs.wuerth.com>
2019-01-15 16:29:40 +0100

    [feature] added backend calls for voting

--- create-survey/src/main/webapp/controller/Detail.controller.js
+++ create-survey/src/main/webapp/controller/Detail.controller.js
@@ -17,6 +17,8 @@ sap.ui.define([
 	 */
 	return BaseController.extend("dhbw.mosbach.neuendorf03.create-survey.controller.Detail", {
 
+		surveyId : "",
+
 		 /**
 		 * Initialize master page.
 		 * @author Eric Schuster WI16C
@@ -39,7 +41,7 @@ sap.ui.define([
                 legend: {
                     title: {
                         visible: false
-                    }
+					}
                 },
                 title: {
                     text: this.getModel("i18n").getProperty("pieChartTitle")
@@ -48,8 +50,18 @@ sap.ui.define([
 					dataLabel: {
 						visible: true
 					}
+				},
+				interaction: {
+					selectability: {
+						mode: "EXCLUSIVE"
+					}
 				}
-            });
+			});
+
+			sap.viz.ui5.api.env.Format.numericFormatter(sap.viz.ui5.format.ChartFormatter.getInstance());
+			var oPopOver = this.getView().byId("idPopOver");
+            oPopOver.connect(oVizFrame.getVizUid());
+            oPopOver.setFormatString(sap.viz.ui5.format.ChartFormatter.DefaultPattern.STANDARDFLOAT);
 
 		},
 
@@ -112,19 +124,111 @@ sap.ui.define([
 			});
 		},
 
+/* ############################ -voting functions- ##################################### */
+/* ##################################################################################### */
+
+		 /**
+		 * Listner. Triggered when a item of the coices list is pressed.
+		 * Triggers selection.
+		 * @author Eric Schuster WI16C
+		 * @memberof dhbw.mosbach.neuendorf03.create-survey.controller.Detail
+		 * @function onCoicesList
+		 * @param {sap.ui.base.Event} oEvent - Event Object of the press action, provided by the framework.
+		 */
+		onCoicesList: function (oEvent) {
+			this.getView().byId("idChooseChoicesList").setSelectedItem(oEvent.getSource(), !oEvent.getSource().getSelected());
+		},
+
+		 /**
+		 * Listner. Triggered when save button is pressed.
+		 * Checks for selection.
+		 * Opens "are you shure?" dialog.
+		 * @author Eric Schuster WI16C
+		 * @memberof dhbw.mosbach.neuendorf03.create-survey.controller.Detail
+		 * @function onSaveChoices
+		 */
+		onSaveChoices: function () {
+			if ( this.getView().byId("idChooseChoicesList").getSelectedItems().length < 1 ) {
+				MessageBox.error(
+					this.getModel("i18n").getProperty("atLeastOne")
+				);
+				return;
+			}
+			var mParameters, aDeferredGroup, oEntry = {}, aItems;
+			//set batch id to group all creates of one survey
+			aDeferredGroup = this.getModel("remote").getDeferredGroups();
+			aDeferredGroup.push("batchCreate");
+			this.getModel("remote").setDeferredGroups(aDeferredGroup);
+			mParameters = { groupId:"batchCreate" };
+
+
+
+
+			aItems = this.getView().byId("idChooseChoicesList").getSelectedItems();
+			for ( var i = 0; i < aItems.length; i++ ) {
+				oEntry = {};
+				oEntry = aItems[i].getBindingContext("remote").getObject();
+				this.getModel("remote").update("/ChoiceSet(Choiceid='" +
+				oEntry.Choiceid + "',Surveyid='" +
+				oEntry.Surveyid + "')", oEntry, mParameters);
+			}
+
+			oEntry = {};
+			oEntry.Userid	= "dummy";
+			oEntry.Surveyid	= this.surveyId;
+			this.getModel("remote").create("/VotedSet", oEntry, mParameters);
+
+			//fire batch
+			this.getModel("remote").submitChanges({
+				groupId: "batchCreate",
+				success: function() {
+					MessageBox.success(
+						this.getModel("i18n").getProperty("successVote")
+					);
+					this._manageVis(true);
+					this.getModel("remote").refresh();
+				}.bind(this),
+				error: function() {
+					MessageBox.error(
+						this.getModel("i18n").getProperty("errorVote")
+					);
+				}.bind(this)
+			});
+
+		},
+
 
 /* ############################ -helper functions- ##################################### */
 /* ##################################################################################### */
 
+		 /**
+		 * Event.
+		 * Triggered via even bus.
+		 * Sets description.
+		 * @author WN00096217 (Eric Schuster)
+		 * @memberof wui.fre.ui5.bedarfsschein-cockpit.controller.Detail
+		 * @function _setDesc
+		 * @param {String} sChannel - channel name.
+		 * @param {String} sEvent - event name.
+		 * @param {String} sDesc - Description to be set.
+		 */
 		_setDesc: function(sChannel, sEvent, sDesc) {
 			this.getView().byId("idTextDesc").setText(sDesc);
 		},
 
-
+		 /**
+		 * Listner. Triggered when the route is matched with the detail view pattern specified in the manifest.
+		 * Manages visibility.
+		 * Manages bindings.
+		 * @author Eric Schuster WI16C
+		 * @memberof dhbw.mosbach.neuendorf03.create-survey.controller.Detail
+		 * @function _onRouteMatched
+		 * @param {sap.ui.base.Event} oEvent - Event Object of the press action, provided by the framework.
+		 */
 		_onRouteMatched: function (oEvent) {
 
 			//close detail column if called via bookmark @todo bookmarkable
-			if (this.getView().byId("idTextDesc").getText() == "") {
+			if (this.getView().byId("idTextDesc").getText() == "" || this.surveyId == "") {
 				this.onColumnCloseButton();
 			}
 
@@ -133,8 +237,8 @@ sap.ui.define([
 				sPath = oEvent.getParameter("arguments")["?query"].Path,
 				oList = this.getView().byId("idChooseChoicesList"),
 				template = new sap.m.StandardListItem({
+					press	: [this.onCoicesList, this],
 					title	: '{remote>Choicetxt}',
-					info 	: '{remote>Votes}',
 					type	: "Active"
 				});
 			oList.bindItems( "remote>/" + sPath  + "/ChoiceSet" , template);
@@ -178,29 +282,44 @@ sap.ui.define([
 					} else {
 						bHasVoted = false;
 					}
-					if ( this.getModel("roleModel").getProperty("/isAdm") ) {
-						oList.setVisible(false);
-						this.getView().byId("idVizFrame").setVisible(true);
-					} else {
-						oList.setVisible(!bHasVoted);
-						this.getView().byId("idVizFrame").setVisible(bHasVoted);
-					}
+					this._manageVis(bHasVoted);
 					}.bind(this),
 				error: function(oError) {
 					oList.setVisible(false);
+					this.getView().byId("idSaveButton").setVisible(false);
 					this.getView().byId("idVizFrame").setVisible(false);
 					MessageBox.error(
 						this.getModel("i18n").getProperty("errorCheckVoted")
 					);
-				 }
+				}.bind(this)
 			});
 
+			this.surveyId = sPath.replace("')", "").replace("SurveySet('", "");
 
+		},
 
+		 /**
+		 * Helper funktion.
+		 * Manages visibility.
+		 * @author Eric Schuster WI16C
+		 * @memberof dhbw.mosbach.neuendorf03.create-survey.controller.Detail
+		 * @function _manageVis
+		 * @param {Boolean} bHasVoted - Event Object of the press action, provided by the framework.
+		 */
+		_manageVis: function (bHasVoted) {
 
-
+			if ( this.getModel("roleModel").getProperty("/isAdm") ) {
+				this.getView().byId("idChooseChoicesList").setVisible(false);
+				this.getView().byId("idSaveButton").setVisible(false);
+				this.getView().byId("idVizFrame").setVisible(true);
+			} else {
+				this.getView().byId("idChooseChoicesList").setVisible(!bHasVoted);
+				this.getView().byId("idSaveButton").setVisible(!bHasVoted);
+				this.getView().byId("idVizFrame").setVisible(bHasVoted);
+			}
 
 		}
+
 
 	});
 }, true);
